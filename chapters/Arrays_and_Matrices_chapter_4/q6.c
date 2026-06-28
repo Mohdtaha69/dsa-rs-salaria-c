151371b89c30cdc46d1cdf97e79785b04dd908a1
MohdTaha699 <292449602+MohdTaha699@users.noreply.github.com>
2026-06-28 06:40:51 +0530

    q6 and q7

---
+++ chapters/Arrays_and_Matrices_chapter_4/q6.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+int sumofmainelement(int ** arr,int r,int c){
+    int sum = 0;
+    for(int i =0; i<r; i++){
+        for(int j =0 ;j<c; j++){
+            if(i<j && j!=c){
+                sum += arr[i][j];
+            }
+        }
+    }
+    return sum;
+}
+int main(){
+    printf("enter the value of rows and coloms: ");
+    int r,c;
+    scanf("%d %d",&r,&c);
+    int ** mat = (int **)malloc(r*sizeof(int*));
+    if(mat==NULL){
+        printf("allocation failed");
+        free(mat);
+        return 1;
+    }
+    for(int i =0; i<r; i++){
+        mat[i]= (int*)malloc(c* sizeof(int));
+        if(mat[i]==NULL){
+            for(int j=0; j<=i; j++){
+                free(mat[j]);
+            }
+            free(mat);
+            return 1;
+        }
+    }
+    // inputting 
+    for(int i=0 ; i<r; i++){
+        for(int j=0; j<c; j++){
+            scanf("%d",&mat[i][j]);
+        }
+    }
+    int res =  sumofmainelement(mat,r,c);
+    printf("%d",res);
+    return 0;
+}
