79f3f1fb9470bcd7744e61ef657ae1a84977b342
MohdTaha699 <292449602+MohdTaha699@users.noreply.github.com>
2026-06-29 16:40:05 +0530

    q10

--- chapters/Arrays_and_Matrices_chapter_4/q10.c
+++ chapters/Arrays_and_Matrices_chapter_4/q10.c
@@ -1,10 +1,39 @@
 #include<stdio.h>
-int isSymmetric(int arr[][],int n);
+int isSymmetric(int n,int arr[n][n]);
 int main(){
+    int n =0;
+    printf("enter the n: ");
+    scanf("%d",&n);
+    int arr [n][n];
+    for(int i =0; i<n; i++){
+        for(int j=0; j<n; j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+
+    int  res = isSymmetric(n,arr);
+    if(res){
+        printf("yes");
+    }
+    else{
+        printf("no");
+    }
 
     return 0;
 }
-int isSymmetric(int arr[][],int n){
+int isSymmetric(int n, int arr[n][n]){
     int check = 1;
-    
+    for(int i =0; i<n; i++){
+        for(int j =0; j<n; j++){
+            if(arr[i][j]==arr[j][i]){
+                continue;
+            }
+            else{
+                check = 0;
+                return check;
+                break;
+            }
+        }
+    }
+    return check;
 }
